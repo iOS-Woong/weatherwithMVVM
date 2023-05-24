daa790ab564f98f1029111757da816024b7a41fb
woong <workplayhard1@naver.com>
2023-05-24 19:04:42 +0900

    feat: EndPoint 타입 생성 (조회하려는 도시에 맞춰서 API통신에 필요한 URL을 생성해줌)

---
+++ Weather/Weather/Service/EndPoint.swift
@@ -0,0 +1,56 @@
+//
+//  EndPoint.swift
+//  Weather
+//
+//  Created by 서현웅 on 2023/05/24.
+//
+
+import Foundation
+
+enum Scheme: String {
+    case https = "https"
+}
+
+enum Host: String {
+    case base = "api.openweathermap.org"
+}
+
+enum Path: String {
+    case base = "/data/2.5/weather"
+}
+
+enum QueryItem: String {
+    case seoul = "seoul"
+    case busan = "busan"
+    case incheon = "incheon"
+    case daegu = "daegu"
+    case daejeon = "daejeon"
+    case gwhangju = "gwhangju"
+    case ulsan = "ulsan"
+    case sejong = "sejong"
+    
+    var description: [URLQueryItem] {
+        let queryItems = [
+            URLQueryItem(name: "q", value: self.rawValue),
+            URLQueryItem(name: "appid", value: "44aea5632c5f4c27d89bbe765acbed69")
+        ]
+        
+        return queryItems
+    }
+}
+
+enum key: String {
+    case key = "&appid=44aea5632c5f4c27d89bbe765acbed69"
+    
+}
+
+struct EndPoint {
+    func url(city query: QueryItem) -> URL? {
+        var component = URLComponents()
+        component.scheme = Scheme.https.rawValue
+        component.host = Host.base.rawValue
+        component.path = Path.base.rawValue
+        component.queryItems = query.description
+        return component.url
+    }
+}
