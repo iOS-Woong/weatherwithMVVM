0ad84c5702828526318928b284cfbd3b6f9ec32b
woong <workplayhard1@naver.com>
2023-05-24 12:19:15 +0900

    feat: Project 생성 및 타겟설정 14.0

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 서현웅 on 2023/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
