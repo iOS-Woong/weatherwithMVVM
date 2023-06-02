201a242f5ddd34e94cc4de8159218615e92172a4
woong <workplayhard1@naver.com>
2023-06-02 19:26:25 +0900

    test_서울URL요청시_상태코드200번이_들어오는지() 함수 테스트

--- Weather/Tests/Repository/NetworkRepositoryTests/NetworkRepositoryTests.swift
+++ Weather/Tests/Repository/NetworkRepositoryTests/NetworkRepositoryTests.swift
@@ -10,12 +10,34 @@ import XCTest
 
 final class NetworkRepositoryTests: XCTestCase {
 
+    var sut: URLSession!
+    let endPoint = EndPoint()
+    
     override func setUpWithError() throws {
+        sut = URLSession(configuration: .default)
     }
 
     override func tearDownWithError() throws {
-
+        sut = nil
     }
 
+    func test_서울URL요청시_상태코드200번이_들어오는지() {
+        // given
+        let url = endPoint.url(city: .seoul)!
+        let promise = expectation(description: "Status Code 200")
+        // when
+        let task = sut.dataTask(with: url) { data, response, error in
+            guard let response = response as? HTTPURLResponse else {
+                return
+            }
+            //then
+            let statusCode = response.statusCode
+            XCTAssertEqual(statusCode, 200)
+            promise.fulfill()
+        }
+        task.resume()
+        
+        wait(for: [promise], timeout: 10)
+    }
     
 }
