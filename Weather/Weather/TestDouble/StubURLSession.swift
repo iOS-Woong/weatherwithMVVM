b43197af905269ff4af4d137f888f63c22eb7744
woong <workplayhard1@naver.com>
2023-06-02 20:27:45 +0900

    feat: StubURLSession의 전반적인 수정(desc상세 기록사항 확인할것)
    - DummyData: URLSession이 네트워킹을 통해 받아오는 data, response, error를 담아주려는 구조체 (값이 없을수있으므로 옵셔널처리)
    - StubURLSession: 테스트 중 URLSession을 대체할 객체, URLSessionProtocol을 채택하여  URLSessionDataTask의 초기화와 동시에 값을 전달할 수 있음.
    - StubURLSessionDataTask: URLSessionDataTask를 상속받은 객체로 테스트 중에 URLSessionDataTask를 대체할 객체이. resume을 통해 DummyData의 completion() 호출하고 결과적으로 dummyData의 프로퍼티를 갖고 온다.

--- Weather/Weather/TestDouble/StubURLSession.swift
+++ Weather/Weather/TestDouble/StubURLSession.swift
@@ -8,12 +8,39 @@
 import Foundation
 
 struct DummyData {
-    let data: Data
-    let response: URLResponse
-    let error: Error
-    let completionHandler: DataTaskCompletionHandler? = nil
+    let data: Data?
+    let response: URLResponse?
+    let error: Error?
+    var completionHandler: DataTaskCompletionHandler? = nil
     
     func completion() {
         completionHandler?(data, response, error)
+    }
+}
+
+class StubURLSession: URLSessionProtocol {
+    let dummyData: DummyData?
+    
+    init(dummyData: DummyData) {
+        self.dummyData = dummyData
+    }
+    
+    
+    func dataTask(with url: URL,
+                  completionHandler: @escaping DataTaskCompletionHandler) -> URLSessionDataTask {
+        return StubURLSessionDataTask(dummyData: dummyData, completionHandler: completionHandler)
+    }
+}
+
+class StubURLSessionDataTask: URLSessionDataTask {
+    var dummyData: DummyData?
+    
+    init(dummyData: DummyData?, completionHandler: DataTaskCompletionHandler?) {
+        self.dummyData = dummyData
+        self.dummyData?.completionHandler = completionHandler
+    }
+    
+    override func resume() {
+        dummyData?.completion()
     }
 }
