97b0a7db11f51b84ea8d17cc311a67b8cce6c2bc
woong <workplayhard1@naver.com>
2023-06-11 01:45:39 +0900

    feat: weatherCollectionView 생성 및 섹션 정의

--- Weather/Weather/View/MainView/WeatherViewController.swift
+++ Weather/Weather/View/MainView/WeatherViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    enum Seciton {
+        case hourly
+        case city
+        case wind
+        case tempMap
+        case detail
+    }
     
     private let viewModel: MainViewModel
+    
+    private let weatherCollectionView = {
+        let collectioniView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
         
+        collectioniView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectioniView
+    }()
+    
     init(viewModel: MainViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -22,7 +37,18 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        setupViews()
     }
     
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(weatherCollectionView)
+        
+        NSLayoutConstraint.activate([
+            weatherCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            weatherCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            weatherCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            weatherCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
