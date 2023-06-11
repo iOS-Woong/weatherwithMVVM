e7c983c56871a9ce4c98580462db4c9934e6f57e
woong <workplayhard1@naver.com>
2023-06-12 07:22:04 +0900

    test: fetch 함수 Test: FiveDayWeatherForecast 타입의 데이터 가공 테스트

--- Weather/Tests/Service/NetworkServiceTests/NetworkServiceTests.swift
+++ Weather/Tests/Service/NetworkServiceTests/NetworkServiceTests.swift
@@ -6,14 +6,35 @@
 //
 
 import XCTest
+@testable import Weather
+
 
 final class NetworkServiceTests: XCTestCase {
 
+    var sut: NetworkService!
+    let endPoint = EndPoint()
+    
     override func setUpWithError() throws {
+        sut = NetworkService()
     }
 
     override func tearDownWithError() throws {
-
+        sut = nil
     }
     
+    // MARK: fetch 함수 Test: [FiveDayWeatherForecast 타입의 데이터 process]
+    
+    func test_fetch_서울을_fetch했을때_cityName을_String으로_가져오는가() {
+        let url = endPoint.url(city: .seoul, for: .forecast)!
+        let seoulString = "seoul"
+                
+        sut.fetch(url: url, type: FiveDayWeatherForecast.self) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertEqual(data.city.name, seoulString)
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+        }
+    }
 }
