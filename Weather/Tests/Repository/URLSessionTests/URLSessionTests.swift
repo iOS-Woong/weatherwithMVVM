ac22d75c9c74cf02c5cf6825c7af59513192a2fc
woong <workplayhard1@naver.com>
2023-06-12 06:31:38 +0900

    test: CurrentWeatherDataAPI HealthCheck 8개도시의 날씨에 대한 API를 연쇄로 요청했을 때 응답이 정상적으로 오는지 테스트

--- Weather/Tests/Repository/URLSessionTests/URLSessionTests.swift
+++ Weather/Tests/Repository/URLSessionTests/URLSessionTests.swift
@@ -9,14 +9,14 @@ import XCTest
 @testable import Weather
 
 final class URLSessionTests: XCTestCase {
-
+    
     var sut: URLSession!
     let endPoint = EndPoint()
     
     override func setUpWithError() throws {
         sut = URLSession(configuration: .default)
     }
-
+    
     override func tearDownWithError() throws {
         sut = nil
     }
@@ -24,15 +24,13 @@ final class URLSessionTests: XCTestCase {
     // MARK: CurrentWeatherDataAPI HealthCheck
     
     func test_CurrentWeatherDataAPI_HealthCheck_하나의_도시대상() {
-        // promise
+        // given
         let url = endPoint.url(city: .seoul, for: .weather)!
-        let promise = expectation(description: "[API: Current Weather] HealthCheck!")
+        let promise = expectation(description: "[API: Current Weather] 단수호출 HealthCheck!")
         
         // when
         let task = sut.dataTask(with: url) { _, response, _ in
-            guard let response = response as? HTTPURLResponse else {
-                return
-            }
+            guard let response = response as? HTTPURLResponse else { return }
             // then
             let statusCode = response.statusCode
             XCTAssertEqual(statusCode, 200)
@@ -42,10 +40,36 @@ final class URLSessionTests: XCTestCase {
         
         wait(for: [promise], timeout: 10)
     }
-
-    func test_FiveDayWeatherForecast_HealthCheck() {
+    
+    func test_CurrentWeatherDataAPI_HealthCheck_전체_도시대상_연쇄호출() {
+        // given
+        let allCityQueryItems : [QueryItem] = QueryItem.allCases // 8개 도시
+        var allResponse = [Int]()
+        let promise = expectation(description: "[API: Current Weather] 8개도시 연쇄호출 HealthCheck!")
         
+        for city in allCityQueryItems {
+            let url = self.endPoint.url(city: city, for: .weather)!
+            
+            let task = self.sut.dataTask(with: url) { _, response,  _ in
+                guard let response = response as? HTTPURLResponse else { return }
+                
+                let statusCode = response.statusCode
+                allResponse.append(statusCode)
+                
+                // then
+                if allResponse.count == 8 {
+                    promise.fulfill()
+                    XCTAssertEqual(allResponse, [200, 200, 200, 200,    200, 200, 200, 200])
+                }
+            }
+            task.resume()
+        }
+        wait(for: [promise], timeout: 10)
     }
     
     
+    // MARK:  API: 5dayWeatherForecastAPI HealthCheck
+    func test_FiveDayWeatherForecast_HealthCheck() {
+        
+    }
 }
