ed47512f33e7896fd74418100ed67ed2adce4251
woong <workplayhard1@naver.com>
2023-06-12 13:38:20 +0900

    test: NetworkServiceTests 완료

--- Weather/Tests/Service/NetworkServiceTests/NetworkServiceTests.swift
+++ Weather/Tests/Service/NetworkServiceTests/NetworkServiceTests.swift
@@ -45,18 +45,20 @@ final class NetworkServiceTests: XCTestCase {
     
     // MARK: fetch 함수 Test: [CurrentWeather 타입의 데이터 process]
     // 테스트범위: [서울 / 부산 / 인천 / 대구 / 대전 / 광주 / 울산 / 세종 ] 8개도시
-    // 1차: [전체도시-decodeError]
-    // 2차: [서울-O / 부산-O / 인천-O / 대구-O / 대전-O / 광주-X / 울산-X / 세종-O]
+    // 1차: [전체도시-decodeError]                                               원인: 타입오류
+    // 2차: [서울-O / 부산-O / 인천-O / 대구-O / 대전-O / 광주-X / 울산-X / 세종-O]     원인: 타입오류
+    // 3차: [서울-O / 부산-O / 인천-O / 대구-O / 대전-O / 광주-O / 울산-O / 세종-O]
+
     
     func test_fetch_서울을_fetch했을때_CurrentWeather타입의_name을_seoul로_가져오는가() {
-        let url = endPoint.url(city: .sejong, for: .weather)!
-        let promise = expectation(description: "[fetch & Precess Test]")
+        let url = endPoint.url(city: .ulsan, for: .weather)!
+        let promise = expectation(description: "[단수도시 fetch & Precess Test]")
         
         sut.fetch(url: url, type: CurrentWeather.self) { result in
             switch result {
             case .success(let data):
                 print("부산나와야하는데?", data.name)
-                XCTAssertEqual(data.name, "Busan")
+                XCTAssertEqual(data.name, "Ulsan")
                 promise.fulfill()
             case .failure(let error):
                 XCTFail("\(error)")
@@ -67,22 +69,25 @@ final class NetworkServiceTests: XCTestCase {
     }
     
     func test_fetch_전체도시를_연속으로_fetch했을때_CurrentWeather타입의_name을_전부_가져오는가() {
-        var allCityName = Set<[String]>()
-        let promise = expectation(description: "전체도시 연쇄호출 및 데이터가공 테스트")
+        var allCityName = Set<String>()
+        let promise = expectation(description: "[전체도시 fetch & Process Test]")
         
         for city in QueryItem.allCases {
             let url = endPoint.url(city: city, for: .weather)!
-            
+
             sut.fetch(url: url, type: CurrentWeather.self) { result in
                 switch result {
                 case .success(let data):
                     let cityName = data.name
-                    allCityName.insert([cityName])
+                    allCityName.insert(cityName)
                     
                     if allCityName.count == 8 {
                         
-                        let setAllCity = Set(arrayLiteral: QueryItem.allCases.map { $0.rawValue })
-                        XCTAssertEqual(allCityName, setAllCity.union(allCityName))
+                        let setAllCity = Set(QueryItem.allCases.map { $0.rawValue.capitalized })
+                        print("난 세트 올씨티얌", setAllCity)
+                        print("난 세트 allCityName이얌", allCityName)
+                        
+                        XCTAssertEqual(allCityName, setAllCity.intersection(allCityName))
                         promise.fulfill()
                     }
                 case .failure(let error):
