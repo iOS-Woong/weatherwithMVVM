73d7b27f06d99cfd5312380f1e32801837f4b7fe
woong <workplayhard1@naver.com>
2023-06-13 17:29:43 +0900

    feat: fetchAllCitiesCurrentWeather 함수 초기 구현 및 전달할 모델 정의

--- Weather/Weather/Service/DataTransferService.swift
+++ Weather/Weather/Service/DataTransferService.swift
@@ -12,7 +12,30 @@ struct DataTransferService {
     private let service = NetworkService()
     
     func fetchAllCitiesCurrentWeather(completion: @escaping ([CityWeather]) -> Void) {
-                    
+        let dispatchGroup = DispatchGroup()
+        var citiesWeather: [CityWeather] = []
+        
+        for city in QueryItem.allCases {
+            dispatchGroup.enter()
+            let url = endPoint.url(city: city, for: .weather)
+            service.fetch(url: url, type: CurrentWeather.self) { result in
+                switch result {
+                case .success(let weather):
+                    let cityWeather = CityWeather(coordinate: Coordinate(lon: weather.coord.lon, lat: weather.coord.lat),
+                                                  temparature: Temparature(temp: weather.main.temp, tempMin: weather.main.tempMin, tempMax: weather.main.tempMax),
+                                                  description: weather.weather[0].description,
+                                                  wind: WindInfo(speed: weather.wind.speed, deg: weather.wind.deg))
+                    citiesWeather.append(cityWeather)
+                case .failure(let error):
+                    print(error)
+                }
+                dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            completion(citiesWeather)
+        }
     }
     
     func fetchFiveDaysForecast(completion: @escaping ([Forecast]) -> Void) {
@@ -46,16 +69,25 @@ struct Forecast {
     let icon: String
 }
 
-struct CityWeather { // TODO: 사용할 데이터에 맞게 타입을 수정해야함
-    let name: String
-    let description : String
+struct CityWeather { // TODO: 필요한 프로퍼티가 더 있다면 추가해야함.
+    let coordinate: Coordinate
+    let temparature: Temparature
+    let description: String
+    let wind: WindInfo
 }
 
-/*
- 
- dtTxt
- main → temp
- 
- weather → icon
- 
- */
+struct Coordinate {
+    let lon: Double
+    let lat: Double
+}
+
+struct Temparature {
+    let temp: Double
+    let tempMin: Double
+    let tempMax: Double
+}
+
+struct WindInfo {
+    let speed: Double
+    let deg: Int
+}
