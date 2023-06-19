35a7add7a7ff9279bc5cb610f0796000405c2b16
woong <workplayhard1@naver.com>
2023-06-19 11:02:11 +0900

    test: DiskStorage에 insert 후, 해당URL에 파일이 존재하는지 test

--- Weather/Tests/DataSource/DiskStorageTests/DiskStorageTests.swift
+++ Weather/Tests/DataSource/DiskStorageTests/DiskStorageTests.swift
@@ -19,18 +19,47 @@ final class DiskStorageTests: XCTestCase {
     
     func test_diskStorage_init했을때_의도한URL에_Directory가_생성되었는지() {
         // given
-        let directoryName = "ForTestDirectory"
-        sut = try! DiskStorage(directoryName: directoryName)
+        let directoryName = "ForTestDirectory_First"
         
         // when
+        sut = try! DiskStorage(directoryName: directoryName)
         
         // then
         guard let url = fileManager.urls(for: .documentDirectory,
                                          in: .userDomainMask).first else { return }
         let directory = url.appendingPathComponent(directoryName)
-
+        print(directory.path)
         let result = fileManager.fileExists(atPath: directory.path)
         
         XCTAssertTrue(result)
+    }
+    
+    func test_diskStorage_insert했을때_Directory에_파일이생성되었는지() {
+        // given
+        let directoryName = "ForTestDirectory_Second"
+        sut = try! DiskStorage(directoryName: directoryName)
+        
+        // when
+        let mock = createJsonMock()
+       
+        sut.insert(mock, for: "FirstKey2")
+       
+                
+        // then
+        guard let url = fileManager.urls(for: .documentDirectory,
+                                         in: .userDomainMask).first else { return }
+        var directory = url.appendingPathComponent(directoryName)
+        directory.appendPathComponent("FirstKey2")
+        
+        print(directory.path) // terminal에서 파일위치 open . 찍어보는용도 -- For macOS Simulator Only
+        
+        let result = fileManager.fileExists(atPath: directory.path)
+        
+        XCTAssertTrue(result)
+    }
+    
+    private func createJsonMock() -> Data {
+        let mockDictionary = ["UIImage": "TestImage"]
+        return try! JSONSerialization.data(withJSONObject: mockDictionary)
     }
 }
