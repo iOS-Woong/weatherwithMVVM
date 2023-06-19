f4aa9dbd50e31b141275a67b39a7b4328392ce2d
woong <workplayhard1@naver.com>
2023-06-19 09:32:36 +0900

    feat: DiskStorage가 init 될 경우, 기본적으로 Directory를 생성하도록 설정

--- Weather/Weather/DataSource/DiskStorage.swift
+++ Weather/Weather/DataSource/DiskStorage.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
+enum DiskStorageError: Error {
+    case canNotFoundDocumentDirectory
+    case canNotCreateStorageDirectory(path: String)
+    case storeError(path: String)
+    case removeError(path: String)
+    case canNotLoadFileList(path: String)
+}
+
 class DiskStorage {
+    private let fileManager: FileManager
+    private let directory: URL
     
+    
+    init(fileManager: FileManager = FileManager.default, directoryName: String) throws {
+        guard let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { throw DiskStorageError.canNotFoundDocumentDirectory }
+        self.fileManager = fileManager
+        self.directory = documentDirectory.appendingPathComponent(directoryName)
+        try createDirectory(with: self.directory)
+    }
+    
+    
+    private func createDirectory(with url: URL) throws {
+        guard !fileManager.fileExists(atPath: url.path) else { return }
+        
+        do {
+            try fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            throw DiskStorageError.canNotCreateStorageDirectory(path: url.path)
+        }
+    }
 }
