5d6afa45f3e3611ccc2bab6d437f2ab1498e3e94
woong <workplayhard1@naver.com>
2023-06-19 12:37:05 +0900

    test: object 함수가 데이터를 키에 맞는 값을 올바르게 가져오는지 테스트 및 잘못된 키값이 들어갔을 때, 오류를 제대로 뱉는지 테스트

--- Weather/Tests/DataSource/DiskStorageTests/DiskStorageTests.swift
+++ Weather/Tests/DataSource/DiskStorageTests/DiskStorageTests.swift
@@ -58,6 +58,45 @@ final class DiskStorageTests: XCTestCase {
         XCTAssertTrue(result)
     }
     
+    func test_diskStorage_object했을때_파일을가져오는지() {
+        // given
+        let directoryName = "ForTestDirectory_Third"
+        let keyName = "key"
+        
+        sut = try! DiskStorage(directoryName: directoryName)
+        let mock = createJsonMock()
+        sut.insert(mock, for: keyName)
+
+        // when
+        let result = try! sut.object(keyName)
+        
+        // then
+        XCTAssertEqual(result, mock)
+    }
+    
+    func test_diskStorage_잘못된키값으로_object했을때_에러를뱉는지() {
+        // given
+        let directoryName = "ForTestDirectory_Fourth"
+        let keyName = "key"
+        let mismatchedKey = "mismatchedKey"
+        
+        sut = try! DiskStorage(directoryName: directoryName)
+        let mock = createJsonMock()
+        sut.insert(mock, for: keyName)
+
+        // when
+        
+        guard let url = fileManager.urls(for: .documentDirectory,
+                                         in: .userDomainMask).first else { return }
+        let directory = url.appendingPathComponent(directoryName)
+        let filePath = directory.appendingPathComponent(mismatchedKey)
+
+        // then
+        XCTAssertThrowsError(try sut.object(mismatchedKey)) { error in
+            XCTAssertEqual(error as? DiskStorageError, DiskStorageError.canNotLoadFile(path: filePath.path))
+        }
+    }
+    
     private func createJsonMock() -> Data {
         let mockDictionary = ["UIImage": "TestImage"]
         return try! JSONSerialization.data(withJSONObject: mockDictionary)
