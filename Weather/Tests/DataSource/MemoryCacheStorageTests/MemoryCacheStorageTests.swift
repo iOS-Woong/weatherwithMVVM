14cb17440b7aa1b11b20c495e972a16157556720
woong <workplayhard1@naver.com>
2023-06-20 15:31:08 +0900

    test: MemoryStorageTests코드를 수정된 타입 제네릭타입으로 수정

--- Weather/Tests/DataSource/MemoryCacheStorageTests/MemoryCacheStorageTests.swift
+++ Weather/Tests/DataSource/MemoryCacheStorageTests/MemoryCacheStorageTests.swift
@@ -10,10 +10,10 @@ import XCTest
 
 final class MemoryCacheStorageTests: XCTestCase {
     
-    var sut: MemoryCacheStorage!
+    var sut: MemoryCacheStorage<Data>!
     
     override func setUpWithError() throws {
-        sut = MemoryCacheStorage()
+        sut = MemoryCacheStorage<Data>()
     }
     
     override func tearDownWithError() throws {
@@ -22,8 +22,8 @@ final class MemoryCacheStorageTests: XCTestCase {
     
     func test_cache메모리에_setObject하고_object로_꺼내오면_두값은_동일할지() {
         // given
-        let object: UIImage = UIImage(systemName: "cloud")!
-        let key: String = "Cloud"
+        let key: String = "1"
+        let object: Data = createJsonMock(key: key, stringObject: "firstObject")
         
         // when
         sut.insert(object, for: key)
@@ -37,30 +37,31 @@ final class MemoryCacheStorageTests: XCTestCase {
     func test_cache메모리에_5개를_setObject하고_object를_5회하면_두값은_동일할지() {
         // given
         let keys = ["1", "2", "3", "4", "5"]
-        let images = [
-            UIImage(systemName: "cloud")!,
-            UIImage(systemName: "pencil")!,
-            UIImage(systemName: "pencil.line")!,
-            UIImage(systemName: "eraser")!,
-            UIImage(systemName: "scribble")!
+        let datas = [
+            createJsonMock(key: keys[0], stringObject: "zero"),
+            createJsonMock(key: keys[1], stringObject: "first"),
+            createJsonMock(key: keys[2], stringObject: "second"),
+            createJsonMock(key: keys[3], stringObject: "third"),
+            createJsonMock(key: keys[4], stringObject: "fourth")
         ]
         
-        for (index, key) in keys.enumerated() {
-            if index < images.count {
-                let image = images[index]
-                sut.insert(image, for: key)
-            }
-        }
-        
         // when
-        var resultImages = [UIImage]()
-        
-        for key in keys {
-            guard let cached = sut.object(key) else { return }
-            resultImages.append(cached)
+        for (index, data) in datas.enumerated() {
+            sut.insert(data, for: keys[index])
         }
         
         // then
-        XCTAssertEqual(resultImages, images)
+        var result = [Data]()
+        
+        for key in keys {
+            result.append(sut.object(key)!)
+        }
+        
+        XCTAssertEqual(result, datas)
+    }
+    
+    private func createJsonMock(key: String, stringObject: String) -> Data {
+        let mockDictionary = ["\(key)": "\(stringObject)"]
+        return try! JSONSerialization.data(withJSONObject: mockDictionary)
     }
 }
