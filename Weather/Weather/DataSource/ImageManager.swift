9ece3fe3bfe6c8ef98a728fae488a57a5c56535c
woong <workplayhard1@naver.com>
2023-06-23 13:50:35 +0900

    feat: else if DiskStorage에 값이 있을 때 케이스까지 구현

--- Weather/Weather/DataSource/ImageManager.swift
+++ Weather/Weather/DataSource/ImageManager.swift
@@ -7,48 +7,51 @@
 
 import Foundation
 
-class ImageManager {
-    private let networkManager: NetworkRepository
-    private let memoryCacheStorage: MemoryCacheStorage<Data>
-    private let diskStorage: DiskStorage?
+protocol ImageManagerType {
+    func retriveImage(_ url: String,
+                      completion: @escaping (Result<Data?, Error>) -> Void)
+}
+
+class ImageManager: ImageManagerType {
+    private let memoryCacheStorage: MemoryCacheStorageType
+    private let diskStorage: DiskStorageType?
     
-    init(networkManager: NetworkRepository = NetworkRepository(),
-         memoryCacheStorage: MemoryCacheStorage<Data> = MemoryCacheStorage(),
-         diskStorage: DiskStorage = try! DiskStorage(directoryName: "mainDirectory"))  {
-        self.networkManager = networkManager
-        self.memoryCacheStorage = MemoryCacheStorage()
-        self.diskStorage = try? DiskStorage(directoryName: "ImageFolder")
+    init(memoryCacheStorage: MemoryCacheStorageType = MemoryCacheStorage(),
+         diskStorage: DiskStorageType = try! DiskStorage(directoryName: "mainDirectory")) {
+        self.memoryCacheStorage = memoryCacheStorage
+        self.diskStorage = diskStorage
     }
     
-    func retriveImage(_ url: String, completion: @escaping (Result<Data?, Error>) -> Void) {
+    func retriveImage(_ url: String,
+                      completion: @escaping (Result<Data?, Error>) -> Void) {
         if let data = memoryCacheStorage.object(url) {
             completion(.success(data))
         } else if diskStorage?.isStored(for: url) == true {
             let data = diskStorage?.object(url)
-            
+            memoryCacheStorage.insert(data!, for: url)
             completion(.success(data))
-            memoryCacheStorage.insert(data, for: url)
         } else {
-            return loadImage(url: url) { result in
-                switch result {
-                case .success(let data):
-                    completion(.success(data))
-                case .failure(let error):
-                    completion(.failure(error))
-                }
-            }
+            return
+//            loadImage(url: url) { result in
+//                switch result {
+//                case .success(let data):
+//                    completion(.success(data))
+//                case .failure(let error):
+//                    completion(.failure(error))
+//                }
+//            }
         }
     }
     
-    private func loadImage(url: String, completion: @escaping (Result<Data, Error>) -> Void) {
-        let requestUrl = URL(string: url)
-        networkManager.request(url: requestUrl) { result in
-            switch result {
-            case .success(let data):
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
+//    private func loadImage(url: String, completion: @escaping (Result<Data, Error>) -> Void) {
+//        let requestUrl = URL(string: url)
+//        networkManager.request(url: requestUrl) { result in
+//            switch result {
+//            case .success(let data):
+//                completion(.success(data))
+//            case .failure(let error):
+//                completion(.failure(error))
+//            }
+//        }
+//    }
 }
