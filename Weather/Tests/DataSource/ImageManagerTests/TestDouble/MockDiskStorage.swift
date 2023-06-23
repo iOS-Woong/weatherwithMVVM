501b68b9fd0bc9da15c1278cd24b17afa26fdb58
woong <workplayhard1@naver.com>
2023-06-23 12:02:38 +0900

    feat: MockDiskSTorage 초기구현

---
+++ Weather/Tests/DataSource/ImageManagerTests/TestDouble/MockDiskStorage.swift
@@ -0,0 +1,52 @@
+//
+//  MockDiskStorage.swift
+//  Weather
+//
+//  Created by 서현웅 on 2023/06/23.
+//
+
+@testable import Weather
+import XCTest
+
+class MockDiskStorage: DiskStorageType {
+    
+    private var storage: [String:Data] = [:]
+    
+    private var isStoreKey: String?
+    private var isStoreCallCount: Int = 0
+
+    private var storeValue: Data?
+    private var storeKey: String?
+    private var storeCallCount: Int = 0
+    
+    private var objectKey: String?
+    private var objectCallCount: Int = 0
+    
+    private var removeKey: String?
+    private var removeCallCount: Int = 0
+        
+    func isStored(for key: String) -> Bool {
+        isStoreKey = key
+        isStoreCallCount += 1
+        return storage[key] != nil
+    }
+    
+    func insert(_ object: Data, for key: String) {
+        storeValue = object
+        storeKey = key
+        storeCallCount += 1
+        storage[key] = object
+    }
+    
+    func object(_ key: String) -> Data? {
+        objectKey = key
+        objectCallCount += 1
+        return storage[key]
+    }
+    
+    func remove(_ key: String) throws {
+        removeKey = key
+        removeCallCount += 1
+        storage[key] = nil
+    }
+}
