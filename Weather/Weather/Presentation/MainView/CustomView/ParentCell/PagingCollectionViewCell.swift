7f2d238445fec60361b25c8d71fa882d73f9da99
woong <workplayhard1@naver.com>
2023-06-25 12:03:27 +0900

    feat: CollectionView Parent 셀 생성 및 Attribute 설정

---
+++ Weather/Weather/Presentation/MainView/CustomView/ParentCell/PagingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PagingCollectionViewCell.swift
+//  Weather
+//
+//  Created by 서현웅 on 2023/06/25.
+//
+
+import UIKit
+
+class PagingCollectionViewCell: UICollectionViewCell, ReusableView {
+    
+}
--- Weather/Weather/Presentation/MainView/WeatherViewController.swift
+++ Weather/Weather/Presentation/MainView/WeatherViewController.swift
@@ -11,7 +11,7 @@ class WeatherViewController: UIViewController {
     
     private let viewModel: WeatherViewModel
         
-    private let weatherCollectionView = {
+    private let pagingCollectionView = {
         let collectioniView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
         
         collectioniView.translatesAutoresizingMaskIntoConstraints = false
@@ -31,27 +31,39 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
-//        setupCollectionViewAttributes() 레이아웃 생성함수
-//        configureCollectionViewCellDatasource() dataSource에 item, header 컨피규어함수
-//        configureSupplementaryViewDatasource() header 컨피규어 함수
-//        bind() 데이터바인딩함수
-//        fetch() 데이터 펫치함수
+        setupCollectionViewAttributes()
     }
-
+    
+    private func setupCollectionViewAttributes() {
+        pagingCollectionView.dataSource = self
+    }
 
     private func setupViews() {
         let collectionImage = UIImage(named: "night")
-        weatherCollectionView.backgroundView = UIImageView(image: collectionImage)
-        view.addSubview(weatherCollectionView)
+        pagingCollectionView.backgroundView = UIImageView(image: collectionImage)
+        view.addSubview(pagingCollectionView)
         
         NSLayoutConstraint.activate([
-            weatherCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
-            weatherCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            weatherCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            weatherCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            pagingCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            pagingCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pagingCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pagingCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
+
+extension WeatherViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return QueryItem.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PagingCollectionViewCell.reuseIdentifier,
+                                                      for: indexPath) as? PagingCollectionViewCell ?? PagingCollectionViewCell()
+        return cell
+    }
+}
+
 
 // MARK: Layout Configure
 //extension WeatherViewController {
@@ -59,28 +71,28 @@ class WeatherViewController: UIViewController {
 //        let layout = UICollectionViewCompositionalLayout { sectionNumber, environment -> NSCollectionLayoutSection? in
 //            guard let sectionKind = Section(rawValue: sectionNumber) else { return nil }
 //            var section: NSCollectionLayoutSection?
-//            
+//
 //            switch sectionKind {
 //            case .hourly:
 //                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.2),
 //                                                                    heightDimension: .fractionalHeight(1.0)))
-//                
+//
 //                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1.0),
 //                                                                                 heightDimension: .absolute(150)),
 //                                                               subitems: [item])
 //                group.contentInsets = .init(top: 0, leading: 10, bottom: 30, trailing: 10)
-//                
+//
 //                let headerView = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0),
 //                                                                                               heightDimension: .fractionalHeight(0.4)),
 //                                                                             elementKind: UICollectionView.elementKindSectionHeader,
 //                                                                             alignment: .top)
 //                section = .init(group: group)
 //                section?.boundarySupplementaryItems = [headerView]
-//                
+//
 //                section?.orthogonalScrollingBehavior = .continuous
-//                
+//
 //                return section
-//            
+//
 //            case .city:
 //                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0),
 //                                                                    heightDimension: .fractionalHeight(1.0)))
@@ -95,9 +107,9 @@ class WeatherViewController: UIViewController {
 //                headerView.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
 //                section = .init(group: group)
 //                section?.boundarySupplementaryItems = [headerView]
-//                
+//
 //                return section
-//                
+//
 //            case .wind:
 //                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0),
 //                                                                    heightDimension: .fractionalHeight(1.0)))
@@ -110,11 +122,11 @@ class WeatherViewController: UIViewController {
 //                                                                             alignment: .top)
 //                headerView.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
 //                section = .init(group: group)
-//                
+//
 //                section?.boundarySupplementaryItems = [headerView]
 //                section?.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
 //                return section
-//                
+//
 //            case .tempMap:
 //                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1.0),
 //                                                                    heightDimension: .fractionalHeight(1.0)))
@@ -130,9 +142,9 @@ class WeatherViewController: UIViewController {
 //                section?.boundarySupplementaryItems = [headerView]
 //                section?.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
 //
-//                
+//
 //                return section
-//                
+//
 //            case .detail:
 //                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5),
 //                                                                    heightDimension: .fractionalHeight(1.0)))
@@ -140,11 +152,11 @@ class WeatherViewController: UIViewController {
 //                                                                               heightDimension: .absolute(300)),
 //                                                             subitems: [item])
 //                section = .init(group: group)
-//                
-//                
+//
+//
 //                return section
 //            }
-//        
+//
 //        }
 ////        layout.register(CommonCollectionBackgroundView.self, forDecorationViewOfKind: CommonCollectionBackgroundView.reuseIdentifier)
 //        return layout
@@ -158,7 +170,7 @@ class WeatherViewController: UIViewController {
 //        let cityCollectionViewCellResistration = citySectionItemConfigure()
 //        let windCollectionViewCellResistration = windSectionItemConfigure()
 //        let tempMapCollectionViewCellResistration = tempSectionItemConfigure()
-//        
+//
 //        datasource = Datasource(collectionView: weatherCollectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
 //            guard let sectionKind = Section(rawValue: indexPath.section) else { return nil }
 //            // TODO: 공통으로 들어가는 섹션구성
@@ -180,15 +192,15 @@ class WeatherViewController: UIViewController {
 //            }
 //        })
 //    }
-//    
+//
 //    private func configureSupplementaryViewDatasource() {
 //        let hourlyHeaderViewResistration = hourlySectionHeaderConfigure()
 //        let cityHeaderViewResistration = citySectionHeaderConfigure()
-//        
+//
 //        datasource?.supplementaryViewProvider = { collectionView, elementKind, indexPath in
 //            guard let sectionKind = Section(rawValue: indexPath.section) else { return nil }
 //            var collectionReusableView: UICollectionReusableView?
-//            
+//
 //            switch sectionKind {
 //            case .hourly:
 //                collectionReusableView = collectionView.dequeueConfiguredReusableSupplementary(using: hourlyHeaderViewResistration, for: indexPath)
@@ -201,24 +213,24 @@ class WeatherViewController: UIViewController {
 //            case .detail:
 //                print("A")
 //            }
-//            
+//
 //            return collectionReusableView
 //        }
 //    }
-//    
+//
 //    private func hourlySecitonItemConfigure() -> UICollectionView.CellRegistration<HourlyCollectionViewCell, Any> {
 //        let hourlySectionResistration = UICollectionView.CellRegistration<HourlyCollectionViewCell, Any> { cell, indexPath, itemIdentifier in
 //            guard let itemIdentifier = itemIdentifier as? Forecast else { return }
 //            cell.configure(text: itemIdentifier)
-//            
+//
 //            self.viewModel.fetchWeatherIcon(iconString: itemIdentifier.icon) {
 //                cell.configure(image: $0)
 //            }
 //        }
-//        
+//
 //        return hourlySectionResistration
 //    }
-//    
+//
 //    private func hourlySectionHeaderConfigure() -> UICollectionView.SupplementaryRegistration<hourlyCollectionHeaderView> {
 //        let hourlySectionHeaderResistration = UICollectionView.SupplementaryRegistration<hourlyCollectionHeaderView>(
 //            elementKind: UICollectionView.elementKindSectionHeader) { supplementaryView, elementKind, indexPath in
@@ -226,10 +238,10 @@ class WeatherViewController: UIViewController {
 //                    supplementaryView.configure(data: weather)
 //                }
 //        }
-//        
+//
 //        return hourlySectionHeaderResistration
 //    }
-//    
+//
 //    private func citySectionItemConfigure() -> UICollectionView.CellRegistration<CityCollectionViewCell, Any> {
 //        let citySectionResistration = UICollectionView.CellRegistration<CityCollectionViewCell, Any> { cell, indexPath, itemIdentifier in
 //            guard let itemIdentifier = itemIdentifier as? CityWeather else { return }
@@ -239,37 +251,37 @@ class WeatherViewController: UIViewController {
 //        }
 //        return citySectionResistration
 //    }
-//    
+//
 //    private func citySectionHeaderConfigure() -> UICollectionView.SupplementaryRegistration<CityCollectionHeaderView> {
 //        let citySectionHeaderResistration = UICollectionView.SupplementaryRegistration<CityCollectionHeaderView>(
 //            elementKind: UICollectionView.elementKindSectionHeader) { supplementaryView, elementKind, indexPath in
 //            // TODO: 여기 cityHeader 컨피규어
 //        }
-//        
+//
 //        return citySectionHeaderResistration
 //    }
-//    
+//
 //    private func windSectionItemConfigure() -> UICollectionView.CellRegistration<WindCollectionViewCell, Any> {
 //        let windSectionResistration = UICollectionView.CellRegistration<WindCollectionViewCell,Any> { cell, indexPath, itemIdentifier in
 //            // TODO: 여기 windCell 컨피규어
 //        }
 //        return windSectionResistration
 //    }
-//    
+//
 //    private func tempSectionItemConfigure() -> UICollectionView.CellRegistration<TempMapCollectionViewCell, Any> {
 //        let tempSectionResistration = UICollectionView.CellRegistration<TempMapCollectionViewCell, Any> { cell, indexPath, itemIdentifier in
 //            // TODO: 여기 tempCell 컨피규어
 //        }
-//        
+//
 //        return tempSectionResistration
 //    }
-//    
+//
 //    private func detailSectionItemConfigure() -> UICollectionView.CellRegistration<DetailCollectionViewCell, Any> {
 //        let detailSectionResistration = UICollectionView.CellRegistration<DetailCollectionViewCell, Any> { cell, indexPath, itemIdentifier in
 //            // TODO: detailSection의 복잡도로 인해 컨피규어는 추후 서버통신작업을 마치고 진행함
 //        }
 //        return detailSectionResistration
 //    }
-//    
+//
 //}
 
