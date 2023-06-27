db293ad7d9a73adc06e0cf8e27eedce0a4128f9f
woong <workplayhard1@naver.com>
2023-06-28 00:36:12 +0900

    feat: CommonCollectionBackgroundView 초기구현

---
+++ Weather/Weather/Presentation/MainView/CustomView/CommonView/CommonCollectionBackgroundView.swift
@@ -0,0 +1,43 @@
+//
+//  CommonCollectionBackgroundView.swift
+//  Weather
+//
+//  Created by 서현웅 on 2023/06/28.
+//
+
+import UIKit
+
+class CommonCollectionBackgroundView: UICollectionReusableView, ReusableView {
+    private let backDecorateView = {
+        let view = UIView()
+        
+        view.backgroundColor = .secondarySystemFill
+        view.layer.cornerRadius = 15
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        self.backgroundColor = .black
+        self.addSubview(backDecorateView)
+        
+        NSLayoutConstraint.activate([
+            backDecorateView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+            backDecorateView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
+            backDecorateView.topAnchor.constraint(equalTo: self.topAnchor),
+            backDecorateView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+}
