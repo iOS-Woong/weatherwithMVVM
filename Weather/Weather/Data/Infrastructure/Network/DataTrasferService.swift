78c66327a2162d83315c2608d0ee938b34dba627
woong <workplayhard1@naver.com>
2023-08-08 14:16:01 +0900

    feat: 응답모델을 디코딩하는 레이어를 한겹 더 추가하여, Repository에서 디코드하는 행위를 분리함

---
+++ Weather/Weather/Data/Infrastructure/Network/DataTrasferService.swift
@@ -0,0 +1,43 @@
+//
+//  DataTrasferService.swift
+//  Weather
+//
+//  Created by 서현웅 on 2023/08/07.
+//
+
+import Foundation
+
+enum ProcessDataError: Error {
+    case decodeError(query: URL? = nil)
+    case networkError(NetworkError)
+}
+
+final class DefaultDataTransferService {
+    private let networkService = NetworkService()
+}
+
+extension DefaultDataTransferService {
+    func request<T: Decodable>(url: TargetType, type: T.Type, completion: @escaping (Result<T, ProcessDataError>) -> Void) {
+        networkService.request(url: url.url) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let self else { return }
+                let decodedData = self.decode(with: data, type: type.self)
+                completion(decodedData)
+            case .failure(let error):
+                completion(.failure(.networkError(error)))
+            }
+        }
+    }
+    
+    private func decode<T: Decodable>(with data: Data, type: T.Type) -> Result<T, ProcessDataError> {
+        let jsonDecoder = JSONDecoder()
+        
+        do {
+            let entity = try jsonDecoder.decode(type.self, from: data)
+            return .success(entity)
+        } catch {
+            return .failure(.decodeError())
+        }
+    }
+}
